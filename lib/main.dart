61e1dd7abe586dd630767f4d43531b9c0a29f3e1
sauliyo15 <gc.saul.7@gmail.com>
2023-06-19 20:43:50 +0200

    Mostrar mensaje simple en pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+//Importar el paquete de flutter
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hola Saul'),
+    );
+  }
+}
