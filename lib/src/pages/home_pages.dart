dafafea8f10afa676895b75e007925055e1507e9
sauliyo15 <gc.saul.7@gmail.com>
2023-06-21 11:26:05 +0200

    floatingActionButtonLocation

--- lib/src/pages/home_pages.dart
+++ lib/src/pages/home_pages.dart
@@ -5,17 +5,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AppBar Sauliyo'),
-      ),
-      body: Center(child: Text('Texto Scaffold')),
-      backgroundColor: Colors.blueGrey.shade200,
-      floatingActionButton: FloatingActionButton(
-        onPressed:
-            setMessage, //Llamar al metodo sin parentesis para que no se ejecute inicialmente
-        child: const Icon(Icons.add),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('AppBar Sauliyo'),
+        ),
+        body: Center(child: Text('Texto Scaffold')),
+        backgroundColor: Colors.blueGrey.shade200,
+        floatingActionButton: FloatingActionButton(
+          onPressed:
+              setMessage, //Llamar al metodo sin parentesis para que no se ejecute inicialmente
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
   }
 
   setMessage() {
