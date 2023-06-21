4e64eb9f2189fd6e4121dec34d9041839bf144eb
sauliyo15 <gc.saul.7@gmail.com>
2023-06-21 11:15:57 +0200

    Scaffold en home_pages.dart con appbar y body

--- lib/src/myApp.dart
+++ lib/src/myApp.dart
@@ -1,9 +1,10 @@
 //Importar el paquete de flutter
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/src/pages/home_pages.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Center(child: Text('Hola Saúliyo')));
+    return MaterialApp(home: Center(child: HomePage()));
   }
 }
---
+++ lib/src/pages/home_pages.dart
@@ -0,0 +1,14 @@
+//Importar el paquete de flutter
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AppBar Sauliyo'),
+      ),
+      body: Center(child: Text('Texto Scaffold')),
+    );
+  }
+}
