3aeab32ff15c2db3a56bba3357414decd43da5fa
sauliyo15 <gc.saul.7@gmail.com>
2023-10-20 17:50:12 +0200

    CircleAvatar en Appbar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,21 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar App Bar'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://pbs.twimg.com/media/F84FfjMW4AAlUYN?format=jpg&name=900x900'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 10.0),
+            child:
+                CircleAvatar(child: Text('SGC'), backgroundColor: Colors.white),
+          )
+        ],
       ),
       body: const Center(
         child: Text('Hello Avatar'),
